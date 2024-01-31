7bafd27001395c33ed86123ca4e066b615d4de0c
Gustavo <107815644+GukCM@users.noreply.github.com>
2024-01-30 22:29:16 -0600

    My name added (#7)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const names = ['Carlos Damian','Rodrigo Mata', 'Ivan Estrada','Joshua Ramos'];
+const names = ['Carlos Damian','Rodrigo Mata', 'Ivan Estrada','Joshua Ramos', 'Gustavo Ceballos'];
 
 const printNames = (arr) => {
   arr.forEach((element) => {

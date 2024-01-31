b5759e70af0c822e344ed0ff3925a64fd21c4f98
JokeGp <jguerrero24@ucol.mx>
2024-01-30 22:59:06 -0600

    feat(js): resolved conflicts?

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const names = ['Carlos Damian','Rodrigo Mata', 'Ivan Estrada','Joshua Ramos', 'Gustavo Ceballos'];
+const names = ['Carlos Damian','Rodrigo Mata', 'Ivan Estrada','Joshua Ramos', 'Gustavo Ceballos', "José Enrique"];
 
 const printNames = (arr) => {
   arr.forEach((element) => {

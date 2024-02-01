f4bdfc167ee185885abc797de15bbfc5fa0a9499
MaximilianoReyes <118138878+MaximilianoReyes@users.noreply.github.com>
2024-01-31 19:49:36 -0600

    Add my name to the code (#4)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const names = ['Carlos Damian','Rodrigo Mata', 'Ivan Estrada','Joshua Ramos', 'Gustavo Ceballos'];
+const names = ['Carlos Damian','Rodrigo Mata','Ivan Estrada','Joshua Ramos','Gustavo Ceballos','Maximiliano Reyes'];
 
 const printNames = (arr) => {
   arr.forEach((element) => {
